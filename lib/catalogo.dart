23ba24ff0b848283c1f62ee68669ebd88bc9bcc3
Daniel <bueno.dani.day@gmail.com>
2025-10-09 11:39:10 -0300

    desenvolvendo tela de catalogo

--- lib/catalogo.dart
+++ lib/catalogo.dart
@@ -22,11 +22,59 @@ class CatalogoPag extends StatelessWidget {
                       color: const Color.fromARGB(255, 255, 255, 236),
                       width: MediaQuery.of(context).size.width * 90 /100,
                       height: MediaQuery.of(context).size.height * 80 /100,
-                      child: Text(
-                        textAlign: TextAlign.center,
-                        '''DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD''',
-                      ),
+                      child: 
+                      Column(
+                        children: [
+                            Container(
+                              child: Text(
+                              textAlign: TextAlign.center,
+                              'Catálogo',
+                            ),
+                          ),
+                          Container(
+                            child: Column(
+                              children: [
+                                Container(
+
+                                  child: Row(
+                                    children: [
+                                      Card(context),
+                                      Card(context),
+                                      Card(context),
+                                    ],
+                                  )
+                                )
+                              ],
+                            )
+                          )
+                        ]
+                      )
                     ),
           )
         )
-        );}}
+        );
+        
+        }
+  Widget Card(BuildContext context){
+    return Container(
+      margin: EdgeInsets.only(left: MediaQuery.of(context).size.width * 3 / 100, right: MediaQuery.of(context).size.width * 3 / 100),
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 217, 217, 217),
+        borderRadius: BorderRadius.all(Radius.circular(20))
+      ),
+      child: Column(
+        children: [
+          Container(
+            child: Image.asset('image/logotipo.png'),
+          ),
+          Container(
+            color: Color.fromARGB(255, 155, 43, 31),
+            child: Text('Nome Produto')
+          )
+        ],
+      )
+    );
+  }        
+        
+        
+}
